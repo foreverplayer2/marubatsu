444e84c2e9cbbcfb6da3957ebaa03a4c504978ad
towa <football.keepergrove2@gmail.com>
2024-02-09 19:21:35 +0900

    enemy_actionの追加

--- marubatsu.py
+++ marubatsu.py
@@ -2,6 +2,9 @@ import random
 import time
 import pandas as pd
 
+
+import pandas as pd
+
 field =  pd.DataFrame([['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9']])
@@ -13,11 +16,20 @@ def my_action():
     maru_place = input().split(',')
     maru_yoko = int(maru_place[0])
     maru_tate = int(maru_place[1])
-    
     field[maru_yoko][maru_tate] = 'o'
     print(field)
-
-my_action()
+    
+def enemy_action():
+    batsu_yoko = 0
+    batsu_tate = 0
+    enemy_turn = True
+    while enemy_turn:
+        batsu_yoko = random.randint(0, 2)
+        batsu_tate = random.randint(0, 2)
+        if field[batsu_yoko][batsu_tate] != 'o' and field[batsu_yoko][batsu_tate] != 'x':
+            field[batsu_yoko][batsu_tate] = 'x'
+            enemy_turn = False
+    print(field)
 
 def judge(field):
     for i in range(2):
